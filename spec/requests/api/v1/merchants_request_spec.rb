59c4ea9ab60f74b714a9128f02ad12ce9923e216
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-06 14:30:14 -0700

    test: requests - merchant, factories, model - merchant

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant API' do
+  before(:each) do
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant3 = create(:merchant)
+    @merchant4 = create(:merchant)
+    @merchant5 = create(:merchant)
+  end
+
+  it 'sends a list of merchants' do
+    get api_v1_merchants_path
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+end
