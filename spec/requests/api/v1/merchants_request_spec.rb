70e17a9b1e9f272ced41c60ba15895bdff036060
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-06 14:48:17 -0700

    test: format of JSON response

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -15,12 +15,18 @@ RSpec.describe 'Merchant API' do
     merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_a(Integer)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
