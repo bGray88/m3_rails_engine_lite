436226b0f6e9648dd236bc62ccd95c65c9e92dba
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-06 19:27:43 -0700

    test: response items - single

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -39,4 +39,29 @@ RSpec.describe 'Merchant Items API' do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it 'sends a single item' do
+    get api_v1_item_path(@item1)
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+  end
 end
