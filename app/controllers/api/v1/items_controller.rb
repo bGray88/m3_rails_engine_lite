4bfc217c10588317f450ff7845fcfeaed187726a
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-06 19:28:04 -0700

    feat: response items - single

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,8 @@ class Api::V1::ItemsController < ApplicationController
       render json: ItemSerializer.format_items(Item.all)
     end
   end
+
+  def show
+    render json: ItemSerializer.format_item(Item.find(params[:id]))
+  end
 end
